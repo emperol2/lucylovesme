9622c6f03349773667d91e6150c5ef8dc3e96fc2
Nuttapon <emperol2@gmil.com>
2015-04-04 22:26:16 +0700

    committed Homepage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,118 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:show, :index, :edit, :update]
+  before_action :correct_user, only: [:index, :edit, :update]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        log_in @user
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to edit_user_path, notice: 'Your profile was successfully updated.' }
+        format.json { render :show, status: :ok, location: edit_user_path }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def manage
+    @user = User.find(params[:id])
+  end
+
+  def profile
+    @user = User.find(params[:id])
+  end
+
+  def projects
+    @user = User.find(params[:id])
+  end
+
+  def payment
+    @user = User.find(params[:id])
+  end
+
+  def dashboard
+    @user = User.find(params[:id])
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      #@user = User.find(params[:id])
+      @user = User.find_by_id(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :created_at, :updated_at, :password_digest, :contact_number, :company_name, :city, :country, :company_type, :zipcode, :address, :state, :timezone, :about_company, :website, :paypal_account, :payment_info, :skpye_id, :linkedin_id, :gender, :about_me, :password, :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      #@user = User.find(params[:id])
+      @user = User.find_by_id(params[:id])
+      flash[:danger] = "You are not allow to see other profiles."
+      redirect_to(current_user) unless current_user?(@user)
+    end
+
+end
