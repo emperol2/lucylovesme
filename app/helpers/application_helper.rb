6007199769be27599067f1563497691503610475
Nuttapon <emperol2@gmail.com>
2015-07-11 18:11:09 +0700

    Fixed Title of each pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     current_page?(link_path) ? "active" : ""
   end
 
+  def title(page_title, options={})
+    content_for(:title, page_title.to_s)
+    return content_tag(:h1, page_title, options)
+  end
+
 end
